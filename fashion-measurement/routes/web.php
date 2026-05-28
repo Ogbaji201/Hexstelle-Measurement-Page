895cb2d2cc9abacee09f90040da8989525b88793
Michael Ogbaji <feyibest1@gmail.com>
2026-05-28 10:02:03 +0100

    Fix Vercel Laravel deployment

--- fashion-measurement/routes/web.php
+++ fashion-measurement/routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
-use App\Http\Controllers\MeasurementController;
+// use App\Http\Controllers\MeasurementController;
 
-Route::get('/', [MeasurementController::class, 'index']);
-Route::post('/submit', [MeasurementController::class, 'submit']);
+// Route::get('/', [MeasurementController::class, 'index']);
+// Route::post('/submit', [MeasurementController::class, 'submit']);
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('measurement');
+}); 
